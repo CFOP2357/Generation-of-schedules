6b3cc9fe3df1116c6e8c01dd2a274332bbd520d2
StephanieHervert <stephanie-hermor@outlook.es>
2021-11-09 19:36:17 -0600

    user_interface

--- src/user_interface.py
+++ src/user_interface.py
@@ -7,20 +7,22 @@ def ask_filename_csv(root: Tk):
 
 class UI(object):
 	"""docstring for UI"""
+
 	def __init__(self, generate_function) -> None:
 		self.root = Tk()
-		self.root.title("Generación de horarios")
+		self.root.title("Generación de Horarios")
 
-		self.open_estudiantes_button = Button(self.root, text="Abrir CSV de estudiantes", 
+		self.label_general = Label(self.root, text="Favor de assiganar todos los archivos para generar los horarios.")
+		self.open_estudiantes_button = Button(self.root, text="Abrir CSV de Estudiantes", 
 											  command=self.update_estudiantes_filename)
 		self.open_grupos_button = Button(self.root, text="Abrir CSV de Grupos", 
 										 command=self.update_grupos_filename)
 		self.open_carreras_button = Button(self.root, text="Abrir CSV de Carreras", 
 										   command=self.update_carreras_filename)
-		self.open_materias_button = Button(self.root, text="Abrir CSV de materias", 
+		self.open_materias_button = Button(self.root, text="Abrir CSV de Materias", 
 										   command=self.update_materias_filename)
 
-		self.generate_schedules_button = Button(self.root, text="Generar horarios", 
+		self.generate_schedules_button = Button(self.root, text="Generar Horarios", 
 												command=generate_function)
 
 		self.build_ui()
@@ -41,11 +43,11 @@ class UI(object):
 		self.materias_filename = ask_filename_csv(self.root)
 
 	def build_ui(self) -> None:
-		self.open_estudiantes_button.pack()
-		self.open_grupos_button.pack()
-		self.open_carreras_button.pack()
-		self.open_materias_button.pack()
-		self.generate_schedules_button.pack()
+		self.label_general.grid(ipadx=5,ipady=5, row=0, column=0, columnspan=3, sticky=S+N+E+W)
+		self.open_estudiantes_button.grid(padx=5,pady=4,ipadx=5,ipady=5, row=1, column=0, sticky=W+E)
+		self.open_grupos_button.grid(padx=5,pady=4,ipadx=5,ipady=5, row=2, column=0, sticky=W+E)
+		self.open_carreras_button.grid(padx=5,pady=4,ipadx=5,ipady=5, row=3, column=0, sticky=W+E)
+		self.open_materias_button.grid(padx=5,pady=4,ipadx=5,ipady=5, row=4, column=0, sticky=W+E)
+		self.generate_schedules_button.grid(padx=5,pady=4,ipadx=5,ipady=5, row=5, column=2, sticky=W+E)
 
-		
 		
