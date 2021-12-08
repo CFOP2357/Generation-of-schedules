73643795d659319f524a418f3cf79dbc06475b24
Pablo Badillo <angelortizacar@gmail.com>
2021-12-08 14:13:58 -0600

    prueba debug hehca en main , esta comentada, salio de forma correcta.

--- src/main.py
+++ src/main.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
 	target=interface.run()
 
 
-if(A.posible_inscribir(88,84,13) == True):
-	print("es correcto")
-else:
-    print("es incorrecto")
+#if(A.posible_inscribir(88,84,13) == True):
+#	print("es correcto")
+#else:
+#    print("es incorrecto")
 
-if(A.posible_inscribir(88,84,2) == False):
-	print("es correcto")
-else:
-    print("es incorrecto")
+#if(A.posible_inscribir(88,84,2) == False):
+#	print("es correcto")
+#else:
+#    print("es incorrecto")
